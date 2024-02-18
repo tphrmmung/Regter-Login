762117d85df1d1f6e025d5678deaf45baa1e14cd
tphrmmung <153507902+tphrmmung@users.noreply.github.com>
2024-02-18 14:44:37 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const notFound = require("./middlewares/notFound");
+const errorMiddleware = require("./middlewares/error");
+const authRoute = require("./routes/auth-route");
+const todoRoute = require("./routes/admin_route");
+// const adminReservation =require("./routes/admin_route");
+const authenticate = require("./middlewares/authenticate");
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+//service
+app.use("/auth", authRoute);
+app.use("/admin", todoRoute);
+// app.use("/admin", adminReservation);
+
+
+
+// notFond
+app.use(notFound);
+
+// error
+app.use(errorMiddleware);
+
+let port = process.env.PORT || 8000;
+app.listen(port, () => console.log("Server on Port : ", port));
